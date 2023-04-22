360451f41b6b578034c102dc9355c81f52fdc8a9
indrapriya-python <rksubapriya@gmail.com>
2023-04-22 18:00:19 +0530

    update and delete

--- School/schoolapp/urls.py
+++ School/schoolapp/urls.py
@@ -3,10 +3,15 @@ from . import views
 urlpatterns = [
     path('',views.index),
     path('login/',views.index1),
+    path('login_user/',views.login_user),
+
     path('data/',views.data),
     path('table/',views.table1),
     path('ragistration/',views.ragistration),
     path('contact/', views.contactdetails),
-    path('cont/', views.contact1)
-  
+    path('cont/', views.contact1),
+    path('delete/<int:pk>/', views.delete_user, name="delete"),
+    path("update/<int:id>/", views.update, name="update"),
+    path('update_data/',views.update_data)
+
 ]
--- School/schoolapp/views.py
+++ School/schoolapp/views.py
@@ -1,43 +1,95 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from django.contrib import messages
-from . models import User,Contact
+from .models import User, Contact
+
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
+
 def ragistration(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        password = make_password(request.POST['password'])
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        password = make_password(request.POST["password"])
         if User.objects.filter(email=email).exists():
-            messages.error(request,"your email exists in db")
+            messages.error(request, "your email exists in db")
         else:
-            User.objects.create(name = name, email = email, password = password)
-            return  redirect("/login/")  
+            User.objects.create(name=name, email=email, password=password)
+            return redirect("/login/")
+
 
 def contactdetails(request):
-        return render(request, "contact.html")
+    return render(request, "contact.html")
+
 
 def contact1(request):
-     if request.method == 'POST':
-          First_name = request.POST['firstname']
-          Last_name =request.POST['lastname']
-          Country_name=request.POST['country']
-          subject=request.POST['subject']
-          Contact.objects.create(First_name=First_name,Last_name=Last_name,Country_name=Country_name,subject=subject)
-          return redirect("/contact/")
+    if request.method == "POST":
+        First_name = request.POST["firstname"]
+        Last_name = request.POST["lastname"]
+        Country_name = request.POST["country"]
+        subject = request.POST["subject"]
+        Contact.objects.create(
+            First_name=First_name,
+            Last_name=Last_name,
+            Country_name=Country_name,
+            subject=subject,
+        )
+        return redirect("/contact/")
+
 
 def index1(request):
     return render(request, "login.html")
+
+
+def login_user(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        if User.objects.filter(email=email).exists():
+            obj = User.objects.get(email=email)
+            password = obj.password
+
+            if check_password(password, password):
+                return redirect("/")
+            else:
+                messages.error, HttpResponse("password incurect")
+                return redirect("/data/")
+
+        else:
+            messages.error(request, "email not register")
+            return redirect("/")
+
+
 def data(request):
     user = User.objects.all()
-    return render(request, "data.html",{"user":user})
-def table1(request):
-     data = Contact.objects.all()
-     return render(request, "table.html",{"data":data})
+    return render(request, "data.html", {"user": user})
 
-     
+
+def table1(request):
+    data = Contact.objects.all()
+    return render(request, "table.html", {"data": data})
+
+
+def delete_user(request, pk):
+    User.objects.filter(id=pk).delete()
+    return redirect("/data/")
+
+
+def update(request, id):
+    user = User.objects.get(id=id)
+    return render(request, "update.html", {"user": user})
+
+
+def update_data(
+    request,
+):
+    if request.method == "POST":
+        id = request.POST["id"]
+        name = request.POST["name"]
+        email = request.POST["email"]
+        User.objects.filter(id=id).update(name=name, email=email)
+        return redirect("/data/")
